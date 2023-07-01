879853afcc45136529eb99889087ff2e82a4cb49
RyanTheScholar <Ryan.business.bowen@gmail.com>
2023-06-30 19:38:54 -0500

    added more JavaScript  and readme information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that in the html.
+$(function () {
+var clearButton = $('.clearBtn')
 var saveButton =$('.saveBtn');
 var target = $('#content').children();
 var hour = dayjs().hour();
 var currentday;
 console.log(target);
 console.log(hour);
+// var text =$('textarea');
+// text = '';
 // var current = dayjs().format('dddd, MMMM D - hh:mm');
 // console.log(dayjs().format());
 // var time = $('.timer');
@@ -19,36 +21,75 @@ setInterval(() => {
     // for(i = 0; i < target.length; i++){
     //     console.log(target[i])
     // }
-    $(target).each($(function(element){
-        var el = $(element)
-        var numAttr = parseInt(el.attr('number'));
+    target.each(function (index, element) {
+        var el = element;
+        console.log(element);
+        var numAttr = parseInt($(el).attr("data-number"));
         console.log(numAttr);
+        if (numAttr === hour) {
+          $(el).addClass("present");
+          $(el).removeClass("past");
+          $(el).removeClass("future");
+        } else if (numAttr > hour) {
+          $(el).addClass("future");
+          $(el).removeClass("past");
+          $(el).removeClass("present");
+        } else {
+          $(el).addClass("past");
+          $(el).removeClass("future");
+          $(el).removeClass("present");
+        }
+      });
+      clearButton.on('click', (function(){
+        localStorage.clear();
+      }))
 
-    }))
-  
 
-
-localArr = {
-};
-$(function () {
-    
+// const local = {
+//     key: target.children().attr('id'), 
+//     value: $('textarea').text()
+// };
+// target.each( function (element) {
+//     var textEl = element;
+//     console.log(element);
+//     var textAttr = parseInt($(textEl).attr("text"));
+//     console.log(textAttr);
+// })
+// create a function for targeting each div element then plug my function into my eventlistener
+        
+    // var textEl = element;
+    // console.log(element);
+    // var textAttr = parseInt($(textEl).attr("text"));
+    // console.log(textAttr);
     // console.log(saveButton);
-    saveButton.on("click", function(){
+    saveButton.on("click", (function(index, element){
         // console.log(event.target);
         // console.log($(thisButton).parent())
         var saveText = $(this).parent().children().eq(1).val();
         console.log(saveText);
-        console.log($(this).parent());
+        console.log($(this).parent().attr('data-number'));
+        // console.log($(this).parent());
         // this parents id as opposed to the entire button
-        localStorage.setItem("text", JSON.stringify(saveText));
+        console.log(target);
+        localStorage.setItem($(this).parent().attr('id'), JSON.stringify(saveText));
         
-        console.log(saveTextRetrieve);
         
-        
-    })
-        var saveTextRetrieve = JSON.parse(localStorage.getItem("text"));
-        $(saveButton).parent().children().eq(1).val(saveTextRetrieve);
+    })); 
+        // var saveTextRetrieve = JSON.parse(localStorage.getItem());
+        // saveButton.parent().children().eq(1).val(saveTextRetrieve);
+
+        $('#hour-9').children().eq(1).val(JSON.parse(localStorage.getItem('hour-9')));
+        $('#hour-10').children().eq(1).val(JSON.parse(localStorage.getItem('hour-10')));
+        $('#hour-11').children().eq(1).val(JSON.parse(localStorage.getItem('hour-11')));
+        $('#hour-12').children().eq(1).val(JSON.parse(localStorage.getItem('hour-12')));
+        $('#hour-13').children().eq(1).val(JSON.parse(localStorage.getItem('hour-13')));
+        $('#hour-14').children().eq(1).val(JSON.parse(localStorage.getItem('hour-14')));
+        $('#hour-15').children().eq(1).val(JSON.parse(localStorage.getItem('hour-15')));
+        $('#hour-16').children().eq(1).val(JSON.parse(localStorage.getItem('hour-16')));
+        $('#hour-17').children().eq(1).val(JSON.parse(localStorage.getItem('hour-17')));
+
  });
+        
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -63,7 +104,7 @@ $(function () {
     // current hour in 24-hour time?
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
+    // the values of the corresponding textarea $(el)ements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
